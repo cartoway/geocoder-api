9107c5ce4fab590d7854544b019f9b628a7d175d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-12-16 17:37:22 +0100

    Fix FR BANO load

--- docker/builder/initialize-fr.sh
+++ docker/builder/initialize-fr.sh
@@ -17,8 +17,29 @@ curl "$BANO" > "./addresses-fr/bano.sjson.gz"
 docker-compose exec redis-addok-fr redis-cli FLUSHALL
 
 docker-compose run --rm addok-fr bash -c "\\
+  # Compute all possible postcodes for city
+  # https://github.com/osm-fr/bano/issues/369
+  zcat /addresses/bano.sjson.gz | tr -d \"\\t\" | jq -c 'select(.type==\"city\" or .type==\"town\" or .type==\"village\")' | jq --slurp 'map({(.id): .}) | add' > /addresses/city-orig.json
+  zcat /addresses/bano.sjson.gz | tr -d \"\\t\" | jq -c 'select(.type==\"place\" or .type==\"street\") | {id: .citycode, postcode: .postcode}' | uniq | jq --slurp 'group_by(.id)| map({(.[0].id): {postcode: [.[].postcode] | flatten | unique}}) | add' > /addresses/city-postcode.json
+  jq --slurp -c '.[0] * .[1] | .[]' /addresses/city-orig.json /addresses/city-postcode.json | gzip > /addresses/city.sjson.gz
+
+  # Duplicate entry, one for each postcode
+  # https://github.com/addok/addok/issues/811
+  mv /addresses/city.sjson.gz /addresses/city-multi.sjson.gz && \\
+  zcat /addresses/city-multi.sjson.gz | jq  '. + {postcode: .postcode[]}' | gzip > /addresses/city.sjson.gz
+
+  zcat /addresses/city.sjson.gz | \\
+  tr -d \"\\t\" | \\
+  jq -c '. | select(.type==\"city\" or .type==\"town\" or .type==\"village\") | . + {citycode: (.citycode // .id) }' | \\
+  jq -c 'def mapping: {\"city\":\"municipality\",\"town\":\"municipality\",\"village\":\"municipality\",\"place\":\"locality\",\"street\":\"street\"}; . + {type: mapping[.type]}' | \\
+  addok batch"
+
+# Bano contents invalid JSON, it is why we need tr -d \"\\t\"
+# https://github.com/osm-fr/bano/issues/367
+docker-compose run --rm addok-fr bash -c "\\
   zcat /addresses/bano.sjson.gz | \\
-  jq -c '. + {citycode: (.citycode // .id) }' | \\
+  tr -d \"\\t\" | \\
+  jq -c '. | select(.type==\"place\" or .type==\"street\") | . + {citycode: (.citycode // .id) }' | \\
   jq -c 'def mapping: {\"city\":\"municipality\",\"town\":\"municipality\",\"village\":\"municipality\",\"place\":\"locality\",\"street\":\"street\"}; . + {type: mapping[.type]}' | \\
   jq -c 'del(.housenumbers[]?.id)' | \\
   addok batch"
