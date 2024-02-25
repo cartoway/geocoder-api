9ac4105da9af1d4fcb3c6beec9432c1e70ecbbd5
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-02-25 14:09:24 +0100

    BANO, no more compute all possible postcodes, data fixed

--- docker/builder/initialize-fr.sh
+++ docker/builder/initialize-fr.sh
@@ -18,18 +18,9 @@ cd ..
 docker-compose exec redis-addok-fr redis-cli FLUSHALL
 
 docker-compose run --rm addok-fr bash -c "\\
-  # Compute all possible postcodes for city
-  # https://github.com/osm-fr/bano/issues/369
-  zcat /addresses/bano.sjson.gz | tr -d \"\\t\" | jq -c 'select(.type==\"city\" or .type==\"town\" or .type==\"village\")' | jq --slurp 'map({(.id): .}) | add' > /addresses/city-orig.json
-  zcat /addresses/bano.sjson.gz | tr -d \"\\t\" | jq -c '. | {id: (.citycode // .id), postcode: .postcode}' | uniq | jq --slurp 'group_by(.id)| map({(.[0].id): {postcode: [.[].postcode] | flatten | unique}}) | add' > /addresses/city-postcode.json
-  jq --slurp -c '.[0] * .[1] | .[]' /addresses/city-orig.json /addresses/city-postcode.json | gzip > /addresses/city-multi.sjson.gz
-  rm /addresses/city-orig.json /addresses/city-postcode.json
-
   # Duplicate entry, one for each postcode
   # https://github.com/addok/addok/issues/811
-  zcat /addresses/city-multi.sjson.gz | jq  '. + {postcode: .postcode[]}' | gzip > /addresses/city.sjson.gz
-
-  zcat /addresses/city.sjson.gz | \\
+  zcat /addresses/bano.sjson.gz | \\
   tr -d \"\\t\" | \\
   jq -c '. | select(.type==\"city\" or .type==\"town\" or .type==\"village\") | . + {citycode: (.citycode // .id) }' | \\
   jq -c 'def mapping: {\"city\":\"municipality\",\"town\":\"municipality\",\"village\":\"municipality\",\"place\":\"locality\",\"street\":\"street\"}; . + {type: mapping[.type]}' | \\
