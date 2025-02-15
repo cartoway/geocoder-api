7a404db2675eda79c024fefdc451ea2abae18a5a
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-02-15 19:16:10 +0100

    Support multiple city names at import of addok-fr, and more clean

--- docker/builder/initialize-fr.sh
+++ docker/builder/initialize-fr.sh
@@ -19,18 +19,21 @@ docker compose run --rm addok-fr bash -c "\\
   # Duplicate entry, one for each postcode
   # https://github.com/addok/addok/issues/811
   zcat /addresses/bano.sjson.gz | \\
-  jq -c '. | select(.type==\"city\" or .type==\"town\" or .type==\"village\") | . + {citycode: (.citycode // .id) } | (. + {postcode: .postcode | (if type!=\"array\" then . else .[] end)})' | \\
+  jq -c '. | select(.type==\"city\" or .type==\"town\" or .type==\"village\")' | \\
+  jq -c 'del(.departement) | del(.region)' | \\
+  jq -c '. + {citycode: (.citycode // .id) } | (. + {postcode: .postcode | (if type!=\"array\" then . else .[] end)}) | (. + {city: .city | (if type!=\"array\" then . else .[] end)})' | \\
   jq -c 'def mapping: {\"city\":\"municipality\",\"town\":\"municipality\",\"village\":\"municipality\",\"place\":\"locality\",\"street\":\"street\"}; . + {type: mapping[.type]}' | \\
   addok batch"
 
 docker compose run --rm addok-fr bash -c "\\
   zcat /addresses/bano.sjson.gz | \\
-  jq -c '. | select(.type==\"place\" or .type==\"street\") | . + {citycode: (.citycode // .id) }' | \\
+  jq -c '. | select(.type==\"place\" or .type==\"street\")' | \\
+  jq -c 'del(.departement) | del(.region) | del(.housenumbers[]?.id)' | \\
+  jq -c '. + {citycode: (.citycode // .id) } | (. + {city: .city | (if type!=\"array\" then . else .[] end)})' | \\
   jq -c 'def mapping: {\"city\":\"municipality\",\"town\":\"municipality\",\"village\":\"municipality\",\"place\":\"locality\",\"street\":\"street\"}; . + {type: mapping[.type]}' | \\
-  jq -c 'del(.housenumbers[]?.id)' | \\
   addok batch"
 
-# # Patch BANO
+# Patch BANO
 docker compose run --rm --entrypoint /bin/bash addok-fr -c "ls ./addresses/*.*json | xargs cat | addok batch"
 
 docker compose run --rm addok-fr addok ngrams
